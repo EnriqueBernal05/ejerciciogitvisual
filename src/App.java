6654f221fb496c91219924b53901d620d025d1ed
javiutrera <fjrodriguez@itpfp.com>
2023-12-05 14:03:05 +0100

    3.0

--- src/App.java
+++ src/App.java
@@ -1,33 +1,34 @@
 import java.util.Scanner;
 
+
 public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         String usuario;
+        String preguntas[] = new String [10]; //Aqui creamos el array preguntas. Lo instanciamos como el escaner y le decimos entre los corchetes el tamaño del array
+        String respuestas[] = new String [10];
         int contador = 0;
-        String [] preguntas = {
-            "¿Cuál es la capital de España?",
-            "¿Cuál es la capital de Francia?",
-            "¿Cuál es la capital de Italia?",
-            "¿Cuál es la capital de Alemania?",
-            "¿Cuál es la capital de Japón?",
-            "¿Cuál es la capital de Brasil?",
-            "¿Cuál es la capital de Portugal?",
-            "¿Cuál es la capital de China?",
-            "¿Cuál es la capital de Argentina?",
-            "¿Cuál es la capital de Holanda?"
-        };
-        String [] respuestas = {
-            "Madrid",
-            "París",
-            "Roma",
-            "Berlín",
-            "Tokio",
-            "Brasilia",
-            "Lisboa",
-            "Pekin",
-            "Buenos Aires",
-            "Amsterdam"
-        };
+        preguntas [0]= "¿Cuál es la capital de España?";//Aqui vamos a darle valor a los 10 arrys vacios que tenemos, en encuenta que se empieza en 0
+        preguntas [1]="¿Cuál es la capital de Francia?";
+        preguntas [2]="¿Cuál es la capital de Italia?";
+        preguntas [3]="¿Cuál es la capital de Alemania?";
+        preguntas [4]="¿Cuál es la capital de Japón?";
+        preguntas [5]="¿Cuál es la capital de Brasil?";
+        preguntas [6]="¿Cuál es la capital de Portugal?";
+        preguntas [7]="¿Cuál es la capital de China?";
+        preguntas [8]="¿Cuál es la capital de Argentina?";
+        preguntas [9]= "¿Cuál es la capital de Holanda?";
+
+        respuesta[0]="Madrid";
+        respuesta[1]="París";
+        respuesta[2]="Roma";
+        respuesta[3]="Berlín";
+        respuesta[4]="Tokio";
+        respuesta[5]="Brasilia";
+        respuesta[6]="Lisboa";
+        respuesta[7]="Pekin";
+        respuesta[8]="Buenos Aires";
+        respuesta[9]="Amsterdam";
+        
         for (int i = 0; i < preguntas.length; i++) {
             System.out.println(preguntas[i]);
             System.out.print("Introduce la respuesta correcta");
@@ -42,5 +43,6 @@ public static void main(String[] args) {
         }
         System.out.println("Has acertado: " + contador + " de 10");
         sc.close();
-    }
+}
+
 
